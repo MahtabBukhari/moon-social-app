b7c93dbf5d746ff1fd890c288ec754cf4149eeae
MahtabBukhari <git config --global user.email you@example.com>
2023-01-12 00:17:16 +0500

    authenticate user and store data to mongodbatlas

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,38 @@
-import bcrypt from 'bcrypt'
-import { Jwt } from 'jsonwebtoken'
-import User from '../models/user'
+import bcrypt from "bcrypt";
+import Jwt  from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      friends,
+      picturePath,
+      location,
+      accupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      friends,
+      picturePath,
+      location,
+      accupation,
+      impressions: Math.floor(Math.random() * 10000),
+      viewedProfile: Math.floor(Math.random() * 10000),
+    });
+    const savedUser =await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({error:error.message})
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { register } from './controllers/auth.js'
 
-
+mongoose.set('strictQuery', true);
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url)
 const __dirname= path.dirname(__filename) //these two lines write because type="module" is used
